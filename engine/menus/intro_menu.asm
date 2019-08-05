881fb1fe2134f235a3a12b865394be59f4b468b2
uryur <uryurshingo@gmail.com>
2019-08-05 19:41:23 +0100

    berry pocket

--- engine/menus/intro_menu.asm
+++ engine/menus/intro_menu.asm
@@ -150,6 +150,9 @@ _ResetWRAM:
 
 	ld hl, wNumBalls
 	call .InitList
+	
+	ld hl, wNumBerries
+	call .InitList
 
 	ld hl, wNumPCItems
 	call .InitList
--- wram.asm
+++ wram.asm
@@ -1915,6 +1915,7 @@ wItemsPocketCursor::    db
 wKeyItemsPocketCursor:: db
 wBallsPocketCursor::    db
 wTMHMPocketCursor::     db
+wBerryPocketCursor::    db
 
 wPCItemsScrollPosition::        db
 wPartyMenuScrollPosition::      db ; unused
@@ -1922,6 +1923,7 @@ wItemsPocketScrollPosition::    db
 wKeyItemsPocketScrollPosition:: db
 wBallsPocketScrollPosition::    db
 wTMHMPocketScrollPosition::     db
+wBerryPocketScrollPosition::    db
 
 wSwitchMon::
 wSwitchItem::
@@ -2308,6 +2310,7 @@ wDudeNumItems:: db
 wDudeItems:: ds 2 * 4
 wDudeItemsEnd:: db
 
+wDudeNumBerries::
 wDudeNumKeyItems:: db ; d292
 wDudeKeyItems:: ds 18
 wDudeKeyItemsEnd:: db
@@ -2463,7 +2466,6 @@ wObjectStructsEnd:: ; d6de
 
 wCmdQueue:: ds CMDQUEUE_CAPACITY * CMDQUEUE_ENTRY_SIZE
 
-	ds 40
 
 wMapObjects:: ; d71e
 wPlayerObject:: map_object wPlayer
@@ -2551,6 +2553,11 @@ wKeyItemsEnd::
 wNumBalls:: db ; d8d7
 wBalls:: ds MAX_BALLS * 2 + 1 ; d8d8
 wBallsEnd::
+
+wNumBerries:: db
+wBerries:: ds MAX_BERRIES * 2 + 1
+wBerriesEnd::
+
 
 wNumPCItems:: db
 wPCItems:: ds MAX_PC_ITEMS * 2 + 1 ; d8f1
