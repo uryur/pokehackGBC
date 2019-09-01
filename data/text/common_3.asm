78b625c2ddf964ca02ab7d11d1792be15467f9c1
uryur <uryurshingo@gmail.com>
2019-09-01 16:27:11 +0100

    Reusable TMs

--- data/text/common_3.asm
+++ data/text/common_3.asm
@@ -478,6 +478,12 @@ UnknownText_0x1c4be3::
 UnknownText_0x1c4bfd::
 	text "How many?"
 	done
+	
+
+AlreadyHaveTMText::
+	text "You already have"
+	line "that TM."
+	done
 
 UnknownText_0x1c4c08::
 	text_decimal wItemQuantityChangeBuffer, 1, 2
